4ec6f061b63de403c63c00e394ffe80c8fd67a51
Scott Hurley <scott.hurley@stfc.ac.uk>
2020-12-18 15:08:25 +0000

    Added ability to use named arguments when using cl tool.

--- packages/uows-client-generator/src/index/index.ts
+++ packages/uows-client-generator/src/index/index.ts
@@ -3,6 +3,7 @@
 import * as fs from 'fs';
 
 import * as soap from 'soap';
+import * as yargs from 'yargs';
 
 import {
   constructorTemplate,
@@ -10,15 +11,30 @@ import {
 } from '../construct-functions/constructFunctions';
 import { populateFuncts } from '../create-service-description/createServiceDescription';
 
-const defaultWsdl: string =
-  'https://api.facilities.rl.ac.uk/ws/UserOfficeWebService?wsdl';
-const defaultFilePath: string = './UOWSSoapInterface.ts';
 let wsdlDesc: any;
 
-//Writes the UOWSSoapInterface.ts file to storage
-const generateCode = (obj: any): void => {
-  const filePath: string = process.argv[3] ? process.argv[3] : defaultFilePath;
+//Gets the arguments provided by the user
+const getArgs = () => {
+  return yargs.options({
+    wsdl: {
+      alias: 'w',
+      type: 'string',
+      demandOption: false,
+      default: 'https://api.facilities.rl.ac.uk/ws/UserOfficeWebService?wsdl',
+      description: 'URL or path pointing to WSDL you wish to use',
+    },
+    outputPath: {
+      alias: 'o',
+      type: 'string',
+      demandOption: false,
+      default: './UOWSSoapInterface.ts',
+      description: 'Output path of file defining client interface',
+    },
+  }).argv;
+};
 
+//Writes the UOWSSoapInterface.ts file to storage
+const generateCode = (obj: any, wsdl: string, filePath: string): void => {
   /*eslint quotes: ["error", "single", { "avoidEscape": true }]*/
   fs.writeFileSync(filePath, "import * as soap from 'soap';\n\n");
   fs.appendFileSync(filePath, 'export default class UOWSSoapClient {\n\n');
@@ -27,7 +43,7 @@ const generateCode = (obj: any): void => {
     filePath,
     `private wsdlDesc: any = ${JSON.stringify(wsdlDesc)};\n\n`
   );
-  fs.appendFileSync(filePath, constructorTemplate(defaultWsdl));
+  fs.appendFileSync(filePath, constructorTemplate(wsdl));
 
   Object.keys(obj).forEach((element: string) => {
     fs.appendFileSync(filePath, obj[element]);
@@ -38,17 +54,20 @@ const generateCode = (obj: any): void => {
 
 //Creates the UOWSService.ts file
 const createInterface = (): void => {
-  const wsdlUrl: string = process.argv[2] ? process.argv[2] : defaultWsdl;
+  const args = getArgs();
+  const wsdl: string = args._.length > 0 ? `${args._[0]}` : args.wsdl;
+  const outputPath: string =
+    args._.length > 1 ? `${args._[1]}` : args.outputPath;
 
   soap
-    .createClientAsync(wsdlUrl)
+    .createClientAsync(wsdl)
     .then((client: soap.Client) => {
       wsdlDesc = client.describe();
 
       return populateFuncts(wsdlDesc);
     })
     .then((obj: any) => {
-      generateCode(obj);
+      generateCode(obj, wsdl, outputPath);
     })
     .catch((err: any) => {
       process.exitCode = 1;
