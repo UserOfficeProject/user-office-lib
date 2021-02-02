d8d18555a7f8d560a4483724c4509b4b253ce027
Martin Trajanovski <martin.trajanovski@ess.eu>
2021-02-02 16:00:12 +0100

    add api access validation

--- packages/validation/src/Admin/index.ts
+++ packages/validation/src/Admin/index.ts
@@ -4,3 +4,29 @@ export const setPageTextValidationSchema = Yup.object().shape({
   id: Yup.number().required(),
   text: Yup.string().notRequired(),
 });
+
+export const createApiAccessTokenValidationSchema = Yup.object().shape({
+  name: Yup.string().required(),
+  accessPermissions: Yup.array()
+    .of(Yup.string())
+    .required('You must select at least one query or mutation for access'),
+});
+
+export const updateApiAccessTokenValidationSchema = (
+  isBackendValidation: boolean
+) =>
+  Yup.object().shape({
+    accessTokenId: isBackendValidation
+      ? Yup.string().required()
+      : Yup.string().notRequired(),
+    name: Yup.string().required(),
+    accessPermissions: isBackendValidation
+      ? Yup.string()
+          .min(5)
+          .required('You must select at least one query or mutation for access')
+      : Yup.array()
+          .of(Yup.string())
+          .required(
+            'You must select at least one query or mutation for access'
+          ),
+  });
