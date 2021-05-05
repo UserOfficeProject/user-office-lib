648e17dfa4dc3defb3d7e46ed90657c7d68e3fc3
jekabskarklins <jekabs.karklins@ess.eu>
2021-05-05 14:06:13 +0300

    validating manager

--- packages/validation/src/Instrument/index.ts
+++ packages/validation/src/Instrument/index.ts
@@ -12,7 +12,9 @@ export const updateInstrumentValidationSchema = Yup.object().shape({
   name: Yup.string().required(),
   shortCode: Yup.string().required(),
   description: Yup.string().required(),
-  managerUserId: Yup.number().required(),
+  managerUserId: Yup.number()
+    .positive('Please specify valid beamline manager')
+    .required(),
 });
 
 export const deleteInstrumentValidationSchema = Yup.object().shape({
@@ -34,7 +36,9 @@ export const removeProposalFromInstrumentValidationSchema = Yup.object().shape({
 });
 
 export const assignScientistsToInstrumentValidationSchema = Yup.object().shape({
-  scientistIds: Yup.array(Yup.number()).min(1).required(),
+  scientistIds: Yup.array(Yup.number())
+    .min(1)
+    .required(),
   instrumentId: Yup.number().required(),
 });
 
@@ -49,7 +53,9 @@ export const setAvailabilityTimeOnInstrumentValidationSchema = Yup.object().shap
   {
     callId: Yup.number().required(),
     instrumentId: Yup.number().required(),
-    availabilityTime: Yup.number().min(0).required(),
+    availabilityTime: Yup.number()
+      .min(0)
+      .required(),
   }
 );
 
