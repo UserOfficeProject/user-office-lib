879868dc4f4887bce3d9dbf2263d4745439dbe49
Martin Trajanovski <martin.trajanovski@ess.eu>
2021-07-28 09:40:19 +0200

    make file upload field required

--- packages/validation/src/Questionary/fileUpload.ts
+++ packages/validation/src/Questionary/fileUpload.ts
@@ -3,7 +3,9 @@ import * as Yup from 'yup';
 export const fileUploadQuestionValidationSchema = (field: any) => {
   const config = field.config;
 
-  let schema = Yup.array().of(Yup.object({ id: Yup.string().required() }));
+  let schema = Yup.array().of(
+    Yup.object({ id: Yup.string().required() }).required()
+  );
 
   config.required &&
     (schema = schema.required('This is a required field').min(1));
