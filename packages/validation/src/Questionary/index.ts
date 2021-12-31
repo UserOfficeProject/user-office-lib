5e18eb7598e4861f43b56818a69d8a6593dbe0e6
Jekabs Karklins <58165815+jekabs-karklins@users.noreply.github.com>
2021-12-31 11:46:13 +0100

    fix: validation does not indicate which question needs (#97)
    * Adding multiple choice validation schema

--- packages/validation/src/Questionary/index.ts
+++ packages/validation/src/Questionary/index.ts
@@ -2,6 +2,7 @@ export * from './boolean';
 export * from './date';
 export * from './fileUpload';
 export * from './interval';
+export * from './multipleChoice';
 export * from './numberInput';
 export * from './richTextInput';
 export * from './textInput';
