ab753185d4ba1015c3e238cde653c1ca4134c8c0
Jekabs Karklins <58165815+jekabs-karklins@users.noreply.github.com>
2022-04-05 11:40:27 +0200

    fix(review): make grade comment required #SWAP-2478 (#113)
    * fix(review): make grade comment required #SWAP-2478
    
    * fix(review): bump version
    
    * fix: update lock
    
    * fix: new lock file
    
    * fix: new package json lock
    
    * fix: outdated package-lock
    
    * chore: align version requirements with ci

--- packages/validation/src/Review/index.ts
+++ packages/validation/src/Review/index.ts
@@ -1,7 +1,7 @@
 import * as Yup from 'yup';
 
 export const proposalGradeValidationSchema = Yup.object().shape({
-  comment: Yup.string().max(1000, 'Too long comment').nullable(),
+  comment: Yup.string().max(1000, 'Too long comment').required(),
   grade: Yup.number()
     .min(0, 'Lowest grade is 0')
     .max(10, 'Highest grade is 10')
