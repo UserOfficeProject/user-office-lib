c37186da46651f5192cca46e0cf8121c86b0f7b1
Jekabs Karklins <Jekabs.Karklins@ess.eu>
2022-12-16 15:32:12 +0100

    fix: expose multilogger

--- packages/logger/src/index.ts
+++ packages/logger/src/index.ts
@@ -1,4 +1,5 @@
 export * from './loggers/Logger';
 export * from './loggers/implementation/GrayLogLogger';
 export * from './loggers/implementation/ConsoleLogger';
+export * from './loggers/implementation/MultiLogger';
 export * from './logger';
--- packages/logger/src/logger.ts
+++ packages/logger/src/logger.ts
@@ -1,11 +1,16 @@
 import { ConsoleLogger } from './loggers/implementation/ConsoleLogger';
+import { MultiLogger } from './loggers/implementation/MultiLogger';
 import { Logger } from './loggers/Logger';
 
 const deaultLogger = new ConsoleLogger();
 let logger: Logger = deaultLogger;
 
-function setLogger(newLogger: Logger) {
-  logger = newLogger;
+function setLogger(newLogger: Logger | Logger[]) {
+  if (Array.isArray(newLogger)) {
+    logger = new MultiLogger(newLogger);
+  } else {
+    logger = newLogger;
+  }
 }
 
 export { logger, setLogger };
