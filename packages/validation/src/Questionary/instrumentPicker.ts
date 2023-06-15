72de971f1e7b2e44ffabc955fab1043ab0f15c23
Yoganandan <yoganandan.pandiyan@ess.eu>
2023-06-15 11:18:06 +0200

    fix: Fixed NaN issue during validation.

--- packages/validation/src/Questionary/instrumentPicker.ts
+++ packages/validation/src/Questionary/instrumentPicker.ts
@@ -3,7 +3,12 @@ import * as Yup from 'yup';
 export const instrumentPickerValidationSchema = (field: any) => {
   const config = field.config;
 
-  let schema = Yup.number().positive().integer();
+  // Value can be a number or null, if not required. If required, it must be a number.
+  let schema = Yup.number()
+    .positive()
+    .integer()
+    .nullable()
+    .transform((_, val) => (val === Number(val) ? val : null)); // Need to write custom transform to allow null values, else Yup throws NaN error
 
   if (config.required) {
     schema = schema.required();
