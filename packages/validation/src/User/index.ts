5e21dd10297328c33feb6ded65a93f97a14912a7
Jekabs Karklins <enthusiast.studio@gmail.com>
2024-01-30 19:24:37 +0100

    fix: add user validation schema for backend

--- packages/validation/src/User/index.ts
+++ packages/validation/src/User/index.ts
@@ -118,6 +118,44 @@ export const updateUserValidationSchema = Yup.object().shape({
   telephone_alt: Yup.string().max(50),
 });
 
+export const updateUserValidationBackendSchema = Yup.object().shape({
+  id: Yup.number().required(),
+  firstname: Yup.string().min(2).max(50).notRequired(),
+  middlename: Yup.string().optional().max(50),
+  preferredname: Yup.string().notRequired().max(50),
+  lastname: Yup.string().min(2).max(50).notRequired(),
+  gender: Yup.string().notRequired(),
+  nationality: Yup.number().notRequired(),
+  user_title: Yup.string().notRequired(),
+  email: Yup.string().email().notRequired(),
+  birthdate: Yup.date()
+    .min(new Date(1900, 1, 1), 'You are not that old')
+    .test('DOB', 'You must be at least 18 years old', (value) => {
+      if (!value) {
+        return true;
+      }
+
+      const dateOfBirth = new Date(value);
+      const dateNow = new Date();
+
+      if (dateNow.getFullYear() - dateOfBirth.getFullYear() < 18) {
+        return false;
+      } else {
+        return true;
+      }
+    })
+    .notRequired(),
+  institutionId: Yup.number().notRequired(),
+  department: Yup.string().min(2).max(50).notRequired(),
+  position: Yup.string().min(2).max(50).notRequired(),
+  telephone: Yup.string()
+    .min(2)
+    .max(30)
+    .matches(phoneRegExp, 'telephone number is not valid')
+    .notRequired(),
+  telephone_alt: Yup.string().max(50),
+});
+
 export const updateUserRolesValidationSchema = Yup.object().shape({
   id: Yup.number().required(),
   roles: Yup.array().of(Yup.number()).required(),
