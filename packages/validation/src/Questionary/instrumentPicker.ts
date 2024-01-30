9a6f1de93dd27bd6b9147dda1840eec289e464e3
martintrajanovski <martin.trajanovski@gmail.com>
2024-01-30 17:49:03 +0100

    BREAKING CHANGE: update instrument picker validation to enable multi instrument selection

--- packages/validation/src/Questionary/instrumentPicker.ts
+++ packages/validation/src/Questionary/instrumentPicker.ts
@@ -3,15 +3,10 @@ import * as Yup from 'yup';
 export const instrumentPickerValidationSchema = (field: any) => {
   const config = field.config;
 
-  // Value can be a number or null, if not required. If required, it must be a number.
-  let schema = Yup.number()
-    .positive()
-    .integer()
-    .nullable()
-    .transform((_, val) => (val === Number(val) ? val : null)); // Need to write custom transform to allow null values, else Yup throws NaN error
+  let schema = Yup.array().of(Yup.number()).nullable();
 
   if (config.required) {
-    schema = schema.required();
+    schema = schema.required().min(1);
   }
 
   return schema;
