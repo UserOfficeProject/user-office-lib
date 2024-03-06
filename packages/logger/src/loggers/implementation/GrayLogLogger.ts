4854951323de3a1b554434d5551b06c89a34188d
martintrajanovski <martin.trajanovski@gmail.com>
2024-03-06 13:20:28 +0100

    fix: add the missing type directly into the class

--- packages/logger/src/loggers/implementation/GrayLogLogger.ts
+++ packages/logger/src/loggers/implementation/GrayLogLogger.ts
@@ -4,6 +4,29 @@ import logger, { MessageCallback } from 'gelf-pro';
 import { LEVEL } from '../../enum/Level';
 import { Logger } from '../Logger';
 
+declare module 'gelf-pro' {
+  function info(
+    message: Message,
+    extra?: MessageExtra,
+    callback?: MessageCallback
+  ): void;
+  function warning(
+    message: Message,
+    extra?: MessageExtra,
+    callback?: MessageCallback
+  ): void;
+  function debug(
+    message: Message,
+    extra?: MessageExtra,
+    callback?: MessageCallback
+  ): void;
+  function error(
+    message: Message,
+    extra?: MessageExtra,
+    callback?: MessageCallback
+  ): void;
+}
+
 function extractPropertiesFromJson(
   json: Record<string, unknown>,
   properties: string[]
