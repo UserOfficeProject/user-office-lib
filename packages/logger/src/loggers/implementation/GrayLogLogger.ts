84278d8274d2d8577e9ead0e22f5a24e68495a30
martintrajanovski <martin.trajanovski@gmail.com>
2024-03-06 13:48:46 +0100

    add type to its own file

--- packages/logger/src/loggers/implementation/GrayLogLogger.ts
+++ packages/logger/src/loggers/implementation/GrayLogLogger.ts
@@ -4,29 +4,6 @@ import logger, { MessageCallback } from 'gelf-pro';
 import { LEVEL } from '../../enum/Level';
 import { Logger } from '../Logger';
 
-declare module 'gelf-pro' {
-  function info(
-    message: Message,
-    extra?: MessageExtra,
-    callback?: MessageCallback
-  ): void;
-  function warning(
-    message: Message,
-    extra?: MessageExtra,
-    callback?: MessageCallback
-  ): void;
-  function debug(
-    message: Message,
-    extra?: MessageExtra,
-    callback?: MessageCallback
-  ): void;
-  function error(
-    message: Message,
-    extra?: MessageExtra,
-    callback?: MessageCallback
-  ): void;
-}
-
 function extractPropertiesFromJson(
   json: Record<string, unknown>,
   properties: string[]
