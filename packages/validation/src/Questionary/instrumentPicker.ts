5cb9ebf408583eaaa9c31b85f9be034963b956a5
Martin <martin.trajanovski@ess.eu>
2024-03-28 13:43:21 +0100

    fix(validation): fix instrument picker validation (#192)

--- packages/validation/src/Questionary/instrumentPicker.ts
+++ packages/validation/src/Questionary/instrumentPicker.ts
@@ -1,12 +1,29 @@
 import * as Yup from 'yup';
+import { AnyObject } from 'yup/lib/types';
 
 export const instrumentPickerValidationSchema = (field: any) => {
   const config = field.config;
 
-  let schema = Yup.array().of(Yup.number()).nullable();
+  let schema:
+    | Yup.ArraySchema<
+        Yup.NumberSchema<number | null | undefined>,
+        AnyObject,
+        (number | undefined)[] | null | undefined
+      >
+    | Yup.NumberSchema<number | null | undefined>;
 
-  if (config.required) {
-    schema = schema.required().min(1);
+  if (config.isMultipleSelect) {
+    schema = Yup.array().of(Yup.number()).nullable();
+
+    if (config.required) {
+      schema = schema.required().min(1);
+    }
+  } else {
+    schema = Yup.number().positive().integer().nullable();
+
+    if (config.required) {
+      schema = schema.required();
+    }
   }
 
   return schema;
