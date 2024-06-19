766781e5cda1623a61bf19a81c18d2a3e9fa6381
deepak jaison <deepak.jaison@stfc.ac.uk>
2024-06-19 11:11:07 +0100

    Add size rescrictions to technique schemas

--- packages/validation/src/Technique/index.ts
+++ packages/validation/src/Technique/index.ts
@@ -1,15 +1,29 @@
 import * as Yup from 'yup';
 
 export const createTechniqueValidationSchema = Yup.object().shape({
-  name: Yup.string().required(),
-  shortCode: Yup.string().required(),
+  name: Yup.string()
+    .max(100, 'Name of the technique should be no longer than 100 characters')
+    .required('Name of the technique is required'),
+  shortCode: Yup.string()
+    .max(
+      20,
+      'Short code of the technique should be no longer than 20 characters'
+    )
+    .required('Short code of the technique is required'),
   description: Yup.string().required(),
 });
 
 export const updateTechniqueValidationSchema = Yup.object().shape({
   id: Yup.number().required(),
-  name: Yup.string().required(),
-  shortCode: Yup.string().required(),
+  name: Yup.string()
+    .max(100, 'Name of the technique should be no longer than 100 characters')
+    .required('Name of the technique is required'),
+  shortCode: Yup.string()
+    .max(
+      20,
+      'Short code of the technique should be no longer than 20 characters'
+    )
+    .required('Short code of the technique is required'),
   description: Yup.string().required(),
 });
 
