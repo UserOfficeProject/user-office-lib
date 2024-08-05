2abf40dfe7a43534da1888dfc8f2045b9c6653c9
deepak jaison <deepak.jaison@stfc.ac.uk>
2024-08-05 13:06:13 +0100

    Add checks for technique picker

---
+++ packages/validation/src/Questionary/techniquePicker.ts
@@ -0,0 +1,30 @@
+import * as Yup from 'yup';
+import { AnyObject } from 'yup/lib/types';
+
+export const techniquePickerValidationSchema = (field: any) => {
+  const config = field.config;
+
+  let schema:
+    | Yup.ArraySchema<
+        Yup.NumberSchema<number | null | undefined>,
+        AnyObject,
+        (number | undefined)[] | null | undefined
+      >
+    | Yup.NumberSchema<number | null | undefined>;
+
+  if (config.isMultipleSelect) {
+    schema = Yup.array().of(Yup.number()).nullable();
+
+    if (config.required) {
+      schema = schema.required().min(1);
+    }
+  } else {
+    schema = Yup.number().positive().integer().nullable();
+
+    if (config.required) {
+      schema = schema.required();
+    }
+  }
+
+  return schema;
+};
