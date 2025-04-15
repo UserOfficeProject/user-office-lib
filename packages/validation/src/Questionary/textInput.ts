2ba38194e1cd09b18957808cb513bd9c2111909e
Jekabs Karklins <58165815+jekabs-karklins@users.noreply.github.com>
2025-04-15 13:27:01 +0200

    fix: text input validation fix for isRequired and min length conflict (#211)

--- packages/validation/src/Questionary/textInput.ts
+++ packages/validation/src/Questionary/textInput.ts
@@ -1,14 +1,22 @@
 import * as Yup from 'yup';
 
 export const textInputQuestionValidationSchema = (field: any) => {
-  let schema = Yup.string();
+  let schema = Yup.string().nullable();
   const config = field.config;
   config.required && (schema = schema.required('This is a required field'));
-  config.min &&
-    (schema = schema.min(
-      config.min,
-      `Value must be at least ${config.min} characters`
-    ));
+
+  if (config.min) {
+    schema = schema.test(
+      'min',
+      `Value must be at least ${config.min} characters`,
+      (value) => {
+        if (!value || value.length === 0) return true;
+
+        return value.length >= config.min;
+      }
+    );
+  }
+
   config.max &&
     (schema = schema.max(
       config.max,
