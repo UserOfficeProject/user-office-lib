9a76a227105296eb45162495e9ca7a83f2d6077d
Fredrik Bolmsten <fredrik.bolmsten@ess.eu>
2025-11-07 14:25:06 +0100

    removed unused user attributes

--- packages/validation/src/User/index.ts
+++ packages/validation/src/User/index.ts
@@ -28,7 +28,6 @@ export const createUserValidationSchema = Yup.object().shape({
   firstname: Yup.string().required().min(2).max(50),
   preferredname: Yup.string().notRequired().max(50),
   lastname: Yup.string().required().min(2).max(50),
-  gender: Yup.string().required(),
   user_title: Yup.string().required(),
   email: Yup.string().email().required(),
   password: passwordValidationSchema,
@@ -41,75 +40,16 @@ export const createUserValidationSchema = Yup.object().shape({
       ),
     })
     .notRequired(),
-  birthdate: Yup.date()
-    .min(new Date(1900, 1, 1), 'You are not that old')
-    .test('DOB', 'You must be at least 18 years old', (value) => {
-      // to keep the current behavior after @types/yup upgrade:
-      // if value is `null` or `undefined` return true explicitly
-      // because new Date(null | undefined) evaluates to `Invalid date`
-      // which return NaN for getFullYear()
-      // and Number - NaN < 18 evaluates to false
-      if (!value) {
-        return true;
-      }
-
-      const dateOfBirth = new Date(value);
-      const dateNow = new Date();
-
-      if (dateNow.getFullYear() - dateOfBirth.getFullYear() < 18) {
-        return false;
-      } else {
-        return true;
-      }
-    })
-    .required('Please specify your birth date'),
   institutionId: Yup.number().required(),
-  department: Yup.string().min(2).max(50).required(),
-  position: Yup.string().min(2).max(50).required(),
-  telephone: Yup.string()
-    .min(2)
-    .max(30)
-    .matches(phoneRegExp, 'telephone number is not valid')
-    .required(),
 });
 
 export const updateUserValidationSchema = Yup.object().shape({
   firstname: Yup.string().min(2).max(50).required(),
   preferredname: Yup.string().notRequired().max(50),
   lastname: Yup.string().min(2).max(50).required(),
-  gender: Yup.string().required(),
   user_title: Yup.string().required(),
   email: Yup.string().email().required(),
-  birthdate: Yup.date()
-    .min(new Date(1900, 1, 1), 'You are not that old')
-    .test('DOB', 'You must be at least 18 years old', (value) => {
-      // to keep the current behavior after @types/yup upgrade:
-      // if value is `null` or `undefined` return true explicitly
-      // because new Date(null | undefined) evaluates to `Invalid date`
-      // which return NaN for getFullYear()
-      // and Number - NaN < 18 evaluates to false
-      if (!value) {
-        return true;
-      }
-
-      const dateOfBirth = new Date(value);
-      const dateNow = new Date();
-
-      if (dateNow.getFullYear() - dateOfBirth.getFullYear() < 18) {
-        return false;
-      } else {
-        return true;
-      }
-    })
-    .required('Please specify your birth date'),
   institutionId: Yup.number().required(),
-  department: Yup.string().min(2).max(50).required(),
-  position: Yup.string().min(2).max(50).required(),
-  telephone: Yup.string()
-    .min(2)
-    .max(30)
-    .matches(phoneRegExp, 'telephone number is not valid')
-    .required(),
 });
 
 export const updateUserValidationBackendSchema = Yup.object().shape({
@@ -117,34 +57,9 @@ export const updateUserValidationBackendSchema = Yup.object().shape({
   firstname: Yup.string().min(2).max(50).notRequired(),
   preferredname: Yup.string().notRequired().max(50),
   lastname: Yup.string().min(2).max(50).notRequired(),
-  gender: Yup.string().notRequired(),
   user_title: Yup.string().notRequired(),
   email: Yup.string().email().notRequired(),
-  birthdate: Yup.date()
-    .min(new Date(1900, 1, 1), 'You are not that old')
-    .test('DOB', 'You must be at least 18 years old', (value) => {
-      if (!value) {
-        return true;
-      }
-
-      const dateOfBirth = new Date(value);
-      const dateNow = new Date();
-
-      if (dateNow.getFullYear() - dateOfBirth.getFullYear() < 18) {
-        return false;
-      } else {
-        return true;
-      }
-    })
-    .notRequired(),
   institutionId: Yup.number().notRequired(),
-  department: Yup.string().min(2).max(50).notRequired(),
-  position: Yup.string().min(2).max(50).notRequired(),
-  telephone: Yup.string()
-    .min(2)
-    .max(30)
-    .matches(phoneRegExp, 'telephone number is not valid')
-    .notRequired(),
 });
 
 export const updateUserRolesValidationSchema = Yup.object().shape({
